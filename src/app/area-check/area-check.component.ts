833f80b636399e159bed0858672c5898b89449dc
Dimon <official.dobrosldm@gmail.com>
2019-01-05 00:18:17 +0300

    started working with canvas - frustrating with TypeScript arithmetic

--- src/app/area-check/area-check.component.ts
+++ src/app/area-check/area-check.component.ts
@@ -1,22 +1,49 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-area-check',
   templateUrl: './area-check.component.html',
   styleUrls: ['./area-check.component.css']
 })
-export class AreaCheckComponent {
-  model: any = {};
-  yVal = 0;
+export class AreaCheckComponent implements AfterViewInit {
+  xVal: number;
+  rVal: number;
+  yVal = -5;
+
+  rectColor = '#FF0000';
+  context: CanvasRenderingContext2D;
+
+  @ViewChild('myCanvas') myCanvas;
 
   constructor() {
   }
 
   check() {
-    alert('x:' + this.model.x + ', y:' + this.yVal + ', r:' + this.model.r);
+    alert('x:' + this.xVal + ', y:' + this.yVal + ', r:' + this.rVal);
   }
 
   handleChangeY(e) {
     this.yVal = e.value;
+  }
+
+
+  ngAfterViewInit() {
+    // const canvas = this.myCanvas.nativeElement;
+    // this.context = canvas.getContext('2d');
+    // this.addPoint();
+  }
+
+  addPoint() {
+    const canvas = this.myCanvas.nativeElement;
+    this.context = canvas.getContext('2d');
+
+    requestAnimationFrame(() => {
+      this.addPoint();
+    });
+
+    const ctx = this.context;
+    ctx.clearRect(0, 0, 400, 400);
+    ctx.fillStyle = this.rectColor;
+    ctx.fillRect(200 + this.xVal, 200 - this.yVal, 4, 4);
   }
 }
